188df7a8e1a93e61bcb927c9529902816e81e9c8
Javmir <167805975+javgamma@users.noreply.github.com>
2024-09-23 16:17:05 +0200

    route.js modified

--- app/api/auth/[kindeAuth]/route.js
+++ app/api/auth/[kindeAuth]/route.js
@@ -5,7 +5,13 @@
 
 import { handleAuth } from "@kinde-oss/kinde-auth-nextjs/server";
 
-export const GET = handleAuth();
+// export const GET = handleAuth();
+
+
+export const GET = handleAuth({
+    postLoginRedirectUri: process.env.KINDE_POST_LOGIN_REDIRECT_URL,
+    postLogoutRedirectUri: process.env.KINDE_POST_LOGOUT_REDIRECT_URL,
+  });
 export const POST = handleAuth();
 export const PUT = handleAuth();
 export const DELETE = handleAuth();
