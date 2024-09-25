f66c34be7296dcd50214837e464868e4bd4c2a18
Javmir <167805975+javgamma@users.noreply.github.com>
2024-09-25 17:54:45 +0200

    cards main modified
    cards modified without button, cancel appointment dialog centered, change in appointment cancellation button.

--- app/(route)/my-booking/_components/CancelAppointment.jsx
+++ app/(route)/my-booking/_components/CancelAppointment.jsx
@@ -1,41 +1,46 @@
-import React from 'react'
+import React from "react";
 import {
-    AlertDialog,
-    AlertDialogAction,
-    AlertDialogCancel,
-    AlertDialogContent,
-    AlertDialogDescription,
-    AlertDialogFooter,
-    AlertDialogHeader,
-    AlertDialogTitle,
-    AlertDialogTrigger,
-  } from "@/components/ui/alert-dialog"
-import { Button } from '@/components/ui/button'
-import { TriangleAlert } from 'lucide-react'
-  
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog";
+import { Button } from "@/components/ui/button";
+import { TriangleAlert } from "lucide-react";
+
+const CancelAppointment = ({ onContinueClick }) => {
 
 
-const CancelAppointment = ({onContinueClick}) => {
   return (
-<AlertDialog>
-  <AlertDialogTrigger>
-    <Button className="text-primary border-primary hover:text-red-600 hover:border-red-600"  variant="outline" >Cancelar cita</Button>
-  </AlertDialogTrigger>
-  <AlertDialogContent>
-    <AlertDialogHeader>
-      <AlertDialogTitle >¿Seguro quieres cancelar tu cita?</AlertDialogTitle>
-      <AlertDialogDescription className="flex justify-center gap-2 text-red-500">
-      <TriangleAlert size={18}/> <span className='flex'>Si lo haces, esta acción no se puede revertir</span>
-      </AlertDialogDescription>
-    </AlertDialogHeader>
-    <AlertDialogFooter>
-      <AlertDialogCancel>Mantener cita</AlertDialogCancel>
-      <AlertDialogAction onClick={()=>onContinueClick()}>Continuar</AlertDialogAction>
-    </AlertDialogFooter>
-  </AlertDialogContent>
-</AlertDialog>
+    <AlertDialog>
+      <AlertDialogTrigger>
+        <Button variant="cancel">Cancelar cita</Button>
+      </AlertDialogTrigger>
+      <AlertDialogContent className="flex flex-col items-center">
+        <AlertDialogHeader >
+          <AlertDialogTitle className="flex text-lg">¿Seguro quieres cancelar tu cita?</AlertDialogTitle>
+          <AlertDialogDescription className="flex justify-center gap-2 text-red-500">
+            <TriangleAlert size={18} />{" "}
+            <span className="flex text-lg">
+              Si lo haces, esta acción no se puede revertir
+            </span>
+          </AlertDialogDescription>
+        </AlertDialogHeader>
+        <AlertDialogFooter>
+          <AlertDialogCancel className="bg-primary text-white"
+          >Mantener cita</AlertDialogCancel>
+          <AlertDialogAction onClick={() => onContinueClick()} className="bg-red-500">
+            Cancelar cita
+          </AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
+};
 
-  )
-}
-
-export default CancelAppointment
+export default CancelAppointment;
--- components/ui/button.jsx
+++ components/ui/button.jsx
@@ -18,6 +18,8 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
+        cancel: "text-white underline-offset-4 hover:underline bg-red-500 hover:bg-accent hover:text-accent-foreground hover:border-red-500 hover:border-2 hover:bg-white hover:text-red-500",
+
       },
       size: {
         default: "h-10 px-4 py-2",
